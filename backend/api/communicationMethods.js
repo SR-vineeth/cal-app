5a5f21428e54b10f5beaefc40ffe9f5972deed17
SR-vineeth <srvineeth2004@gmail.com>
2025-01-02 13:29:28 +0530

    api update

--- backend/api/communicationMethods.js
+++ backend/api/communicationMethods.js
@@ -1,6 +1,25 @@
-// /api/communicationMethods.js
-const methodRoutes = require("../routes/communicationMethods");
+const express = require("express");
+const CommunicationMethod = require("../models/communicationMethod");
 
-module.exports = (req, res) => {
-  methodRoutes(req, res);
-};
+const router = express.Router();
+
+// Get all communication methods
+router.get("/", async (req, res) => {
+  const methods = await CommunicationMethod.find().sort("sequence");
+  res.json(methods);
+});
+
+// Add a communication method
+router.post("/", async (req, res) => {
+  const newMethod = new CommunicationMethod(req.body);
+  const savedMethod = await newMethod.save();
+  res.json(savedMethod);
+});
+
+// Delete a communication method
+router.delete("/:id", async (req, res) => {
+  await CommunicationMethod.findByIdAndDelete(req.params.id);
+  res.json({ message: "Communication Method deleted" });
+});
+
+module.exports = router;
